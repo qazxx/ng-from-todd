81ba9c6b283929badffba1dc2723a4cf0a36ec7e
KakumanuShiva.Nagendra <kakumanushiva.nagendra@thomsonreuters.com>
2019-03-02 17:52:41 +0530

    removed and added part 2

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+/*
+This import includes in polyfills.ts needed by Angular and is loaded before the app. For optimization polyfills.ts has been removed.
+Zone JS is required by default for Angular itself.
+*/
